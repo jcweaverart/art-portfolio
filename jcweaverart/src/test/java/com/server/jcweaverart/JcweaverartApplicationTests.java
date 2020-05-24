af2304ca811c9b4d8f73193cdec7024c5a9d1dc6
Jordan Weaver <jweaver@widen.com>
2020-05-23 20:48:50 -0500

    added back end server

---
+++ jcweaverart/src/test/java/com/server/jcweaverart/JcweaverartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.jcweaverart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcweaverartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
